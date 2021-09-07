6d0df596f621ca4ae47d9886224ce96e1ae02054
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-09-07 08:27:55 -0300

    add create-room components and room-details

--- front-SalaDeReuniao/src/app/rooms/create-room/create-room.component.ts
+++ front-SalaDeReuniao/src/app/rooms/create-room/create-room.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Room} from "../room";
+import {RoomService} from "../room.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-room',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRoomComponent implements OnInit {
 
-  constructor() { }
+  room: Room = new Room;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(private roomService: RoomService, private router: Router) { }
+
+  ngOnInit() {
   }
 
+  newRoom(): void {
+    this.submitted = false;
+    this.room = new Room();
+  }
+
+  save() {
+    this.roomService.createRoom(this.room)
+        .subscribe(data => console.log(data), error => console.log(error));
+    this.room = new Room();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
+
+  gotoList() {
+    this.router.navigate(['/rooms']);
+  }
 }
--- front-SalaDeReuniao/src/app/rooms/room-details/room-details.component.ts
+++ front-SalaDeReuniao/src/app/rooms/room-details/room-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Room} from "../room";
+import {ActivatedRoute, Router} from "@angular/router";
+import {RoomService} from "../room.service";
 
 @Component({
   selector: 'app-room-details',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  room: Room;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,private router: Router, private roomService: RoomService) { }
+
+  ngOnInit() {
+    this.room = new Room();
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.roomService.getRoom(this.id)
+        .subscribe(data => {
+          console.log(data)
+          this.room = data;
+        }, error => console.log(error));
   }
 
+  list(){
+    this.router.navigate(['rooms']);
+  }
 }
