a471f8a25038401dd0b42f9399fdd2827639ef4f
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-09-07 08:30:35 -0300

    add room-list.component.ts

--- front-SalaDeReuniao/src/app/rooms/room-list/room-list.component.ts
+++ front-SalaDeReuniao/src/app/rooms/room-list/room-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from "rxjs";
+import {Room} from "../room";
+import {RoomService} from "../room.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
+  rooms: Observable<Room[]>;
 
-  constructor() { }
+  constructor(private roomService: RoomService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.rooms = this.roomService.getRoomsList();
   }
 
+
+  deleteRoom(id: number) {
+    this.roomService.deleteRoom(id)
+        .subscribe(
+            data => {
+              console.log(data);
+              this.ngOnInit();
+            },
+            error => console.log(error));
+  }
+
+  roomDetails(id: number){
+    this.router.navigate(['details', id]);
+  }
+
+  updateRoom(id: number){
+    this.router.navigate(['update', id]);
+  }
 }
