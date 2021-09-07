2cd1bfb7b61b912d0aec58d4190adf7b7eaa5ed6
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-09-07 08:33:45 -0300

    add update-component

--- front-SalaDeReuniao/src/app/rooms/update-room/update-room.component.ts
+++ front-SalaDeReuniao/src/app/rooms/update-room/update-room.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Room} from "../room";
+import {ActivatedRoute, Router} from "@angular/router";
+import {RoomService} from "../room.service";
 
 @Component({
   selector: 'app-update-room',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateRoomComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  room: Room;
+  submitted = false;
 
-  ngOnInit(): void {
+
+  constructor(private route: ActivatedRoute,private router: Router, private roomService: RoomService) { }
+
+  ngOnInit() {
+    this.room = new Room();
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.roomService.getRoom(this.id)
+        .subscribe(data => {
+          console.log(data)
+          this.room = data;
+        }, error => console.log(error));
   }
 
+  updateRoom() {
+    this.roomService.updateRoom(this.id, this.room)
+        .subscribe(data => console.log(data), error => console.log(error));
+    this.room = new Room();
+    this.gotoList();
+  }
+
+  onSubmit() {
+    this.updateRoom();
+  }
+
+  gotoList() {
+    this.router.navigate(['/rooms']);
+  }
 }
